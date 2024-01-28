19c64ab2616e6dddf3db6c617e3aa0432ee00658
Linnea <127832519+Linneaeas@users.noreply.github.com>
2024-01-28 17:12:53 +0100

    All functionality OK

--- script.js
+++ script.js
@@ -1,5 +1,35 @@
 const socket = io("http://localhost:3000"); //Create socket variable and location where the server hosting the socket.
+const messageContainer = document.getElementById("message-container"); //Creating/connecting our message container
+const messageForm = document.getElementById("send-container"); //Creating/connecting our form
+const messageInput = document.getElementById("message-input"); //Creating/connecting message input
+
+const name = prompt("What is your name?"); //Alert to get the users name
+appendMessage("You joined"); //Showing when a name is typed in and user has joined
+socket.emit("new-user", name); //Sending the new user to the server
 
 socket.on("chat-message", (data) => {
-  console.log(data);
-}); //Whenever we recieve an event calling a function with the data that we sent down from the server.
+  appendMessage(`${data.name}: ${data.message}`);
+}); //Calling the function appendMessage when a user sends a chat message, also including the users name.
+
+socket.on("user-connected", (name) => {
+  appendMessage(`${name} connected`); // Calling the function appendMessage when a user is connected
+});
+
+socket.on("user-disconnected", (name) => {
+  appendMessage(`${name} disconnected`); // Calling the function appendMessage when a user disconnect
+});
+
+messageForm.addEventListener("submit", (e) => {
+  e.preventDefault(); //Whenever we submit our form, we wanna stop the form to submitting so the page dont refresh(in that case we would loose all our chatmessages)
+  const message = messageInput.value; //Getting the value from the message input
+  appendMessage(`You: ${message}`); //Include the users own messages
+  socket.emit("send-chat-message", message); //Send information from the client to the server
+  messageInput.value = ""; //Clear input field.
+});
+
+function appendMessage(message) {
+  //Append message to our index file
+  const messageElement = document.createElement("div"); //Create a messageElement
+  messageElement.innerText = message; //Setting the value of the text
+  messageContainer.append(messageElement); //Adding the message to the messageContainer
+}
--- server.js
+++ server.js
@@ -3,8 +3,26 @@ const io = require("socket.io")(3000, {
     origin: "http://127.0.0.1:5500",
     methods: ["GET", "POST"],
   },
-});
+}); //Added to solve cors error
+
+const users = {}; // Creating users as an empty object(that will get the id of the individuals socket)
 
 io.on("connection", (socket) => {
-  socket.emit("chat-message", "Hello World");
-}); //Everytime someone connects to our server, it's gonna call this function and give each user it's own socket and using the socket were sending a message down to the client with this event chat-message.
+  //socket.emit("chat-message", "Hello World");
+  //Everytime someone connects to our server it's gonna call the socket.emit function and give each user it's own socket and using that socket were sending a message down to the client with this event chat-message.
+  socket.on("new-user", (name) => {
+    users[socket.id] = name; //Using the users socket id to become the object user, which will be the users name
+    socket.broadcast.emit("user-connected", name); // Message to the existing members that someone joined.
+  });
+
+  socket.on("send-chat-message", (message) => {
+    socket.broadcast.emit("chat-message", {
+      message: message,
+      name: users[socket.id],
+    }); //Create an function so our server knows how to handle what has been sent from the client. socket.broadcast.emit sending the message to every single person who is connected to that server except the person who sent the message.
+  });
+  socket.on("disconnect", () => {
+    socket.broadcast.emit("user-disconnected", users[socket.id]);
+    delete users[socket.id];
+  });
+});
