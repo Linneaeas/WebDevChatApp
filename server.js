0985c7101c97c3edd1f4b0faddcbb42919bffdf4
Linnea <127832519+Linneaeas@users.noreply.github.com>
2024-01-28 15:03:26 +0100

    Cors error fixed

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const io = require("socket.io")(3000); //Creating a server on port 3000
+const io = require("socket.io")(3000, {
+  cors: {
+    origin: "http://127.0.0.1:5500",
+    methods: ["GET", "POST"],
+  },
+});
 
 io.on("connection", (socket) => {
   socket.emit("chat-message", "Hello World");
